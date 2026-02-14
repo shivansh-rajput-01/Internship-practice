05eba2d6d62533ab42d6889a00b5670075ed767e
shivansh-rajput-01 <shivanshr579@gmail.com>
2026-02-14 17:11:59 +0530

    configured provider in app component to make store available

--- ReduxDemo/src/App.jsx
+++ ReduxDemo/src/App.jsx
@@ -1,11 +1,16 @@
 import './App.css';
+import Todo from "./components/Todo.jsx";
+import { Provider } from 'react-redux';
+import { store } from "./app/store.js";
 
 function App() {
   return (
     <>
-      
+      <Provider store={store}>
+        <Todo />
+      </Provider>
     </>
-  )
+  );
 }
 
 export default App;
